e1e89639b161df1aff707855a4e95a97b91b0e46
Mitchell <mover333@gmail.com>
2017-07-07 11:58:53 -0700

    reformatted fucntions in Data container

--- src/containers/Data.tsx
+++ src/containers/Data.tsx
@@ -3,55 +3,50 @@ import * as React from 'react'
 import { EmailType } from '../interfaces/Email'
 
 export interface DataProps {
-  initData: (email: EmailType) => void
+    initData: (email: EmailType) => void
 }
 
 export class Data extends React.Component<DataProps, any> {
 
-  componentWillMount() {
-    const patientMailData = document.getElementById('config').getAttribute('data-patientmail');
-    const clinicMailData = document.getElementById('config').getAttribute('data-clinicMail');
+    componentWillMount() {
+        const patientMailData = document.getElementById('config').getAttribute('data-patientmail');
+        const clinicMailData = document.getElementById('config').getAttribute('data-clinicMail');
 
-    //dispatch settting emails for both 
-    const initData = this.props.initData;
-    
-    //todo: layout
-    const genEmails = (resObj: any, tag: string): void => {
+        //dispatch settting emails for both 
+        const initData = this.props.initData;
 
-        const parseEmailObj = (emailObj: any, tag2: string, idx: number): EmailType => {
-            let email: EmailType = {
-            tag : tag2,
-            number : idx,
-            sender : emailObj.sender.emailAddress.address,
-            message : emailObj.body.content,
-            subject : emailObj.subject,
-            timeSent : emailObj.receivedDateTime
-            };
-            
+        genEmails(JSON.parse(patientMailData)['value'], 'patient', initData);
+        genEmails(JSON.parse(clinicMailData)['value'], 'clinic', initData);
+    }
 
-            return email;
+    render() {
+        const DivStyle = {
+            display: 'none'
         }
-      const initData = this.props.initData;
-      let x = 0;
-      resObj.forEach((e: any) => {
-        if(e.sender.emailAddress.name != tag) {
+        return <div style={DivStyle}>
+        </div>
+    }
+}
+
+const parseEmailObj = (emailObj: any, tag2: string, idx: number): EmailType => {
+    let email: EmailType = {
+        tag: tag2,
+        number: idx,
+        sender: emailObj.sender.emailAddress.address,
+        message: emailObj.body.content,
+        subject: emailObj.subject,
+        timeSent: emailObj.receivedDateTime
+    };
+
+
+    return email;
+}
+
+const genEmails = (resObj: any, tag: string, initData: (email: EmailType) => void): void => {
+    let x = 0;
+    resObj.forEach((e: any) => {
+        if (e.sender.emailAddress.name != tag) {
             initData(parseEmailObj(e, tag, ++x));
         }
     });
-    }
-
-    genEmails(JSON.parse(patientMailData)['value'], 'patient');
-    genEmails(JSON.parse(clinicMailData)['value'], 'clinic');
-  }
-
-  
-
-  render() {
-      const DivStyle = {
-          display: 'none'
-      }
-      return <div style={DivStyle}>
-
-      </div>
-  }
 }
